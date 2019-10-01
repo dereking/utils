28c776abd0465428c7f979695d34bdf06b189851
dereking <kedereking@gmail.com>
2019-10-01 21:34:36 +0800

    t'

--- StrUtil/time.go
+++ StrUtil/time.go
@@ -1,18 +1,9 @@
-package StrUtil
+﻿package StrUtil
 
-import (
-	"crypto/md5"
-	"encoding/hex"
+import ( 
 	"github.com/noaway/dateparse"
 )
-
-// 生成32位MD5
-func MD5(text string) string {
-	ctx := md5.New()
-	ctx.Write([]byte(text))
-	return hex.EncodeToString(ctx.Sum(nil))
-}
-
+ 
 
 func StrToTime(str string) (time.Time ,error){
 	return  dateparse.ParseAny(dateExample)
