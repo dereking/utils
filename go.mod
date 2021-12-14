da4e71b7a53f12e03b40e6e18406e64f679f2846
Derek <kedereking@gmail.com>
2021-12-14 11:31:22 +0800

    d

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/Dereking/utils
+
+go 1.16
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
-package main
+package utils
 
 import (
-	"github.com/Dereking/utils/strutil"
 	"log"
+
+	"github.com/Dereking/utils/strutil"
 )
 
-func main(){
+func main() {
 
 	log.Println(strutil.StrToTime("19 SEP 2019"))
 }
